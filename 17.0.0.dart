1fca06dd623280284e4efec4ebcb62561b02ed6e
rowjoy202 <rowjoy202@gmail.com>
2022-10-16 19:42:21 +0600

    1800

--- 17.0.0.dart
+++ 17.0.0.dart
@@ -18,7 +18,7 @@ If you enjoyed this kata more advanced and generalized version of it can be foun
 
 [Personal thanks to Professor Jim Fowler on Coursera for his awesome classes that I really recommend to any math enthusiast and for showing me this mathematical curiosity too with his usual contagious passion :)]
 
- */
+*/
 
 void main() {
   _aonacci([1, 1,1], 10);
---
+++ 18.0.0.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+}
+
+String bool_to_word(bool boolean) {
+  // your code goes here
+  String _a = "";
+  if (boolean == true){
+      _a = "Yes";
+  }else if (boolean == false){
+    _a = "No";
+  }
+  return _a;
+}
